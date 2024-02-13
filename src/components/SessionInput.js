ebc921ed86ac041f013f4bb86d1f1cc1d501cd16
stingotho <othoofficial@gmail.com>
2024-02-13 21:41:40 +0300

    session text labels and text area styling

--- src/components/SessionInput.js
+++ src/components/SessionInput.js
@@ -10,38 +10,50 @@ function SessionInput({ sessionDetails, setSessionDetails }) {
   };
 
   return (
-    <div>
+    <div className="space-y-4">
       <h3 className="text-md font-semibold text-gray-900">Session Details</h3>
-      <div>
-        <label htmlFor="objective">Objective:</label>
+      <div className="flex items-center space-x-4">
+        <label htmlFor="objective" className="flex-1 text-right mr-4">
+          Objective:
+        </label>
         <textarea
+          className="flex-2 w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
           id="objective"
           name="objective"
           value={sessionDetails.objective}
           onChange={handleChange}
         />
       </div>
-      <div>
-        <label htmlFor="activities">Learning/Activities:</label>
+      <div className="flex items-center space-x-4">
+        <label htmlFor="activities" className="flex-1 text-right mr-4">
+          Learning/Activities:
+        </label>
         <textarea
+          className="flex-2 w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
           id="activities"
           name="activities"
           value={sessionDetails.activities}
           onChange={handleChange}
         />
       </div>
-      <div>
-        <label htmlFor="resources">Resources/Materials:</label>
+      <div className="flex items-center space-x-4">
+        <label htmlFor="resources" className="flex-1 text-right mr-4">
+          Resources/Materials:
+        </label>
         <textarea
+          className="flex-2 w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
           id="resources"
           name="resources"
           value={sessionDetails.resources}
           onChange={handleChange}
         />
       </div>
-      <div>
-        <label htmlFor="homework">Homework:</label>
+      <div className="flex items-center space-x-4">
+        <label htmlFor="homework" className="flex-1 text-right mr-4">
+          Homework:
+        </label>
         <textarea
+          className="flex-2 w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
           id="homework"
           name="homework"
           value={sessionDetails.homework}
